080081a9a085db907fcc9223953b098f3e45c944
Saksham <skshm.090898@gmail.com>
2024-06-01 17:28:27 +0530

    setup vite + react with material tailwind

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from "@material-tailwind/react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='text-2xl text-blue-800 underline inline-block mx-12 my-12'>Navbar</div>
+      <Button>Button</Button>
+    </div>
+  )
+}
+
+export default Navbar
