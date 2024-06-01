917ba04a1e8a16c1db279ca30423a9a1b096d456
Saksham <skshm.090898@gmail.com>
2024-06-01 17:48:44 +0530

    added react-scroll,framer-motion,some google-fonts package

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        petitForma : "Petit Formal Script",
+        inter : "Inter",
+        russo: "Russo One",
+        roboto : "Roboto"
+      }
+    },
   },
   plugins: [],
 });
