81d56860f31c80ab5c7729e6abfd3aef7b04c736
Saksham <skshm.090898@gmail.com>
2024-06-16 20:24:39 +0530

    updated

--- src/components/ContactUs/Modal.jsx
+++ src/components/ContactUs/Modal.jsx
@@ -13,7 +13,7 @@ const Modal = ({ open, setOpen }) => {
   return (
     <div>
       <motion.div
-        className="sm:right-0 sm:bottom-0 sm:justify-center sm:ml-5 bg-white fixed right-4 bottom-2 z-20 flex flex-col rounded-2xl"
+        className="sm:right-3 sm:bottom-2 bg-white fixed right-4 bottom-2 z-20 flex flex-col rounded-2xl"
         initial={{ scale: 1, opacity: 0, height: 0, width: 0 }}
         animate={{
           x: open ? -30 : 0,
@@ -54,7 +54,7 @@ const Modal = ({ open, setOpen }) => {
           </div>
           <Tooltip content="Close" placement="left">
             <div
-              className=" absolute top-4 right-4 text-white cursor-pointer"
+              className="absolute top-4 right-4 text-white cursor-pointer"
               onClick={() => setOpen(false)}
             >
               <svg
